9b9230f181eb1425f92da26f7f6c4dd01a0f842c
Nguyen Phuc Son Tung <ngphsotu@gmail.com>
2022-11-11 13:52:33 +0700

    new version not use mapEventToState

--- lib/auth/login/login_bloc.dart
+++ lib/auth/login/login_bloc.dart
@@ -8,27 +8,52 @@ import '/auth/login/login_state.dart';
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
   final AuthRepository authRepo;
 
-  LoginBloc({required this.authRepo}) : super(LoginState());
+  // LoginBloc({required this.authRepo}) : super(LoginState());
 
-  Stream<LoginState> mapEventToState(LoginEvent event) async* {
-    // Username updated
+  LoginBloc({required this.authRepo}) : super(LoginState()) {
+    on<LoginEvent>(_onEvent);
+  }
+
+  Future<void> _onEvent(LoginEvent event, Emitter<LoginState> emit) async {
     if (event is LoginUsernameChanged) {
-      yield state.copyWith(username: event.username);
-
-      // Password updated
-    } else if (event is LoginPasswordChanged) {
-      yield state.copyWith(password: event.password);
-
-      // Form submitted
-    } else if (event is LoginSubmitted) {
-      yield state.copyWith(formStatus: FormSubmitting());
+      emit(state.copyWith(username: event.username));
+    }
+    // password update
+    else if (event is LoginPasswordChanged) {
+      emit(state.copyWith(password: event.password));
+    }
+    //form submitted
+    else if (event is LoginSubmitted) {
+      emit(state.copyWith(formStatus: FormSubmitting()));
 
       try {
         await authRepo.login();
-        yield state.copyWith(formStatus: SubmissionSuccess());
+        emit(state.copyWith(formStatus: SubmissionSuccess()));
       } on Exception catch (e) {
-        yield state.copyWith(formStatus: SubmissionFailed(e));
+        emit(state.copyWith(formStatus: SubmissionFailed(e)));
       }
     }
   }
+
+  // Stream<LoginState> mapEventToState(LoginEvent event) async* {
+  //   // Username updated
+  //   if (event is LoginUsernameChanged) {
+  //     yield state.copyWith(username: event.username);
+
+  //     // Password updated
+  //   } else if (event is LoginPasswordChanged) {
+  //     yield state.copyWith(password: event.password);
+
+  //     // Form submitted
+  //   } else if (event is LoginSubmitted) {
+  //     yield state.copyWith(formStatus: FormSubmitting());
+
+  //     try {
+  //       await authRepo.login();
+  //       yield state.copyWith(formStatus: SubmissionSuccess());
+  //     } on Exception catch (e) {
+  //       yield state.copyWith(formStatus: SubmissionFailed(e));
+  //     }
+  //   }
+  // }
 }
